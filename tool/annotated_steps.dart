f0328ec9cc9b326dafe1d6e9bd5ea5c916751a75
Erik Ernst <eernst@google.com>
2018-04-11 15:10:49 +0200

    Bug fix

--- tool/annotated_steps.dart
+++ tool/annotated_steps.dart
@@ -30,7 +30,7 @@ int runBuildRunnerInDirectory(String directory) async {
 // Expects to be called with the path to the package root.
 // See .test_config.
 main(List<String> arguments) async {
-  String packagePath = arguments[1];
+  String packagePath = arguments[0];
   print("^^^ Run 'annotated_steps.dart' in $packagePath");
   int exitCode = await runBuildRunnerInDirectory(packagePath);
   print("^^^ Done 'annotated_steps.dart'");
